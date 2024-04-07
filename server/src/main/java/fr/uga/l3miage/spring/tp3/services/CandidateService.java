7ca8a373c1aea549ff40fcc9e97f6170552fe7ff
l3miage-mansosam <sami.mansour@etu.univ-grenoble-alpes.fr>
2024-04-07 23:50:26 +0200

    Components(valid) -SessionComponentTest (fonctione)

--- server/src/main/java/fr/uga/l3miage/spring/tp3/services/CandidateService.java
+++ server/src/main/java/fr/uga/l3miage/spring/tp3/services/CandidateService.java
@@ -5,7 +5,13 @@ import fr.uga.l3miage.spring.tp3.exceptions.rest.CandidateNotFoundRestException;
 import fr.uga.l3miage.spring.tp3.exceptions.technical.CandidateNotFoundException;
 import fr.uga.l3miage.spring.tp3.models.CandidateEntity;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +22,32 @@ public class CandidateService {
         try {
             CandidateEntity candidateEntity = candidateComponent.getCandidatById(candidateId);
             return (candidateEntity.getCandidateEvaluationGridEntities().stream().reduce(0d, (average, grid) -> average + (grid.getGrade() * grid.getExamEntity().getWeight()), Double::sum))
-                    / candidateEntity.getCandidateEvaluationGridEntities().stream().reduce(0,(acc,grid) -> acc + grid.getExamEntity().getWeight(),Integer::sum);
+                    / candidateEntity.getCandidateEvaluationGridEntities().toArray().length;
         } catch (CandidateNotFoundException e) {
             throw new CandidateNotFoundRestException(e.getMessage(),e.getCandidateId());
         }
     }
 
+    public Boolean addStudentsToTestCenter(Long testCenterId, Set<Long> candidateIds) {
+        boolean testCenterExists = true;
+        if (!testCenterExists) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Le centre de test n'est pas trouvé");
+        }
+
+        for (Long candidateId : candidateIds) {
+            try {
+                CandidateEntity candidate = candidateComponent.getCandidatById(candidateId);
+                LocalDate birthDate = candidate.getBirthDate();
+                int age = Period.between(birthDate, LocalDate.now()).getYears();
+
+                if (age < 18) {
+                    throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Un ou plusieurs étudiants ont moins de 18 ans");
+                }
+            } catch (CandidateNotFoundException e) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Le candidat [%s] n'a pas été trouvé", candidateId));
+            }
+        }
+
+        return true;
+    }
 }
